9a90a967417ecb539054aa0edd4f6f45c97fafea
Wellington <junior.wel02@exemplo.br>
2021-12-08 14:08:55 -0300

    iniciando desafio!

---
+++ README.md
@@ -0,0 +1,1 @@
+#Desafio Donus-Code
---
+++ index.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+const express = require("express");
+
+const PORT= process.env.PORT || 3000;
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
